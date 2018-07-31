42f442e6316f4a6969cb475b6c262d637a5799c9
Evgeny Ivchenko <goldenjoe@outlook.com>
2018-07-31 21:31:16 +0300

    [Graphics] Add the ability to cross-compile GLSL

---
+++ src/NitroSharp.ShaderCompiler/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using SharpDX.D3DCompiler;
+using Veldrid;
+using Veldrid.SPIRV;
+
+namespace NitroSharp.ShaderCompiler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                try
+                {
+                    CompileAll(args[0]);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+
+        private static void CompileAll(string inputDirectory)
+        {
+            string outputDirectory = Path.Combine(inputDirectory, "Generated");
+            Directory.CreateDirectory(outputDirectory);
+            IEnumerable<string> files = Directory.EnumerateFiles(inputDirectory);
+
+            var shaderSets =
+                from path in files
+                let vert = path.EndsWith("vert")
+                let frag = path.EndsWith("frag")
+                where vert || frag
+                let name = Path.GetFileNameWithoutExtension(path)
+                group (path, vert, frag) by name into g
+                where g.Count() == 2
+                select new
+                {
+                    Name = g.Key,
+                    Vertex = g.FirstOrDefault(x => x.vert).path,
+                    Fragment = g.FirstOrDefault(x => x.frag).path,
+                };
+
+            foreach (var shaderSet in shaderSets)
+            {
+                string outputBase = Path.Combine(outputDirectory, shaderSet.Name);
+                byte[] vs = File.ReadAllBytes(shaderSet.Vertex);
+                byte[] fs = File.ReadAllBytes(shaderSet.Fragment);
+
+                string vsSource = Encoding.UTF8.GetString(vs);
+                string fsSource = Encoding.UTF8.GetString(fs);
+
+                var debugCompileOptions = new GlslCompileOptions(debug: true);
+                var vsSpvDebugOutput = SpirvCompilation.CompileGlslToSpirv(
+                    vsSource, string.Empty, ShaderStages.Vertex,
+                    debugCompileOptions);
+                var fsSpvDebugOutput = SpirvCompilation.CompileGlslToSpirv(
+                    fsSource, string.Empty, ShaderStages.Fragment,
+                    debugCompileOptions);
+
+                var releaseCompileOptions = new GlslCompileOptions(debug: false);
+                var vsSpvReleaseOutput = SpirvCompilation.CompileGlslToSpirv(
+                    vsSource, string.Empty, ShaderStages.Vertex,
+                    releaseCompileOptions);
+                var fsSpvReleaseOutput = SpirvCompilation.CompileGlslToSpirv(
+                    fsSource, string.Empty, ShaderStages.Fragment,
+                    releaseCompileOptions);
+                File.WriteAllBytes(outputBase + "-vertex.spv", vsSpvReleaseOutput.SpirvBytes);
+                File.WriteAllBytes(outputBase + "-fragment.spv", fsSpvDebugOutput.SpirvBytes);
+
+                var glCompileOptions = new CrossCompileOptions(fixClipSpaceZ: false, invertVertexOutputY: false);
+                var glslResult = SpirvCompilation.CompileVertexFragment(
+                    vsSpvDebugOutput.SpirvBytes,
+                    fsSpvDebugOutput.SpirvBytes,
+                    CrossCompileTarget.GLSL,
+                    glCompileOptions);
+                File.WriteAllText(outputBase + "-vertex.330.glsl", glslResult.VertexShader);
+                File.WriteAllText(outputBase + "-fragment.330.glsl", glslResult.FragmentShader);
+
+                var esslResult = SpirvCompilation.CompileVertexFragment(
+                    vsSpvDebugOutput.SpirvBytes,
+                    fsSpvDebugOutput.SpirvBytes,
+                    CrossCompileTarget.ESSL,
+                    glCompileOptions);
+                File.WriteAllText(outputBase + "-vertex.300.glsles", glslResult.VertexShader);
+                File.WriteAllText(outputBase + "-fragment.300.glsles", glslResult.FragmentShader);
+
+                var hlslResult = SpirvCompilation.CompileVertexFragment(
+                    vsSpvReleaseOutput.SpirvBytes,
+                    fsSpvReleaseOutput.SpirvBytes,
+                    CrossCompileTarget.HLSL);
+                byte[] vertBytes = Encoding.UTF8.GetBytes(hlslResult.VertexShader);
+                byte[] fragBytes = Encoding.UTF8.GetBytes(hlslResult.FragmentShader);
+
+                File.WriteAllBytes(outputBase + "-vertex.hlsl.bytes", CompileHlsl(ShaderStages.Vertex, vertBytes));
+                File.WriteAllBytes(outputBase + "-fragment.hlsl.bytes", CompileHlsl(ShaderStages.Fragment, fragBytes));
+            }
+        }
+
+        private static byte[] CompileHlsl(ShaderStages stage, byte[] sourceCode)
+        {
+            string profile = stage == ShaderStages.Vertex ? "vs_3_0" : "ps_3_0";
+
+            ShaderFlags flags = ShaderFlags.OptimizationLevel3;
+            CompilationResult result = ShaderBytecode.Compile(
+                sourceCode,
+                "main",
+                profile,
+                flags);
+
+            if (result.ResultCode.Failure)
+            {
+                Console.WriteLine($"Failed to compile HLSL code: {result.Message}");
+                return Array.Empty<byte>();
+            }
+
+            return result.Bytecode.Data;
+        }
+    }
+}
---
+++ src/NitroSharp/Graphics/Shaders/Generated/sprite-fragment.300.glsles
@@ -0,0 +1,12 @@
+#version 330
+
+uniform sampler2D SourceTexture;
+
+layout(location = 0) out vec4 OutputColor;
+in vec2 vdspv_fsin0;
+
+void main()
+{
+    OutputColor = texture(SourceTexture, vdspv_fsin0);
+}
+
