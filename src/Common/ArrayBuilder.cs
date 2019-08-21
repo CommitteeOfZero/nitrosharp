8b545b818b1789c4783abd52960b85a67855c742
Evgeny Ivchenko <goldenjoe@outlook.com>
2019-08-22 00:00:30 +0300

    Rewrite ArrayBuilder

--- src/Common/ArrayBuilder.cs
+++ src/Common/ArrayBuilder.cs
@@ -2,13 +2,14 @@
 using System.Diagnostics;
 using System.Runtime.CompilerServices;
 
+#nullable enable
+
 namespace NitroSharp.Utilities
 {
     internal struct ArrayBuilder<T>
     {
-        private const uint DefaultCapacity = 4;
-
-        public T[] _elements;
+        private T[] _elements;
+        private uint _count;
 
         public ArrayBuilder(int initialCapacity) : this((uint)initialCapacity)
         {
@@ -16,128 +17,127 @@ namespace NitroSharp.Utilities
 
         public ArrayBuilder(uint initialCapacity)
         {
-            _elements = new T[initialCapacity];
-            Count = 0;
+            _elements = initialCapacity > 0
+                ? new T[initialCapacity]
+                : Array.Empty<T>();
+            _count = 0;
         }
 
-        public uint Count;
-
         public T[] UnderlyingArray => _elements;
+        public uint Count => _count;
 
         public ref T this[uint index] => ref _elements[index];
         public ref T this[int index] => ref _elements[index];
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public ref T Add()
         {
-            if (_elements == null)
-            {
-                _elements = new T[DefaultCapacity];
-            }
-
-            if (_elements.Length <= Count)
-            {
-                Array.Resize(ref _elements, (int)Count * 2);
-            }
-
-            return ref _elements[Count++];
+            EnsureCapacity(_count + 1);
+            return ref _elements[_count++];
         }
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public void Add(T item)
         {
-            if (_elements == null)
-            {
-                _elements = new T[DefaultCapacity];
-            }
-
-            if (_elements.Length <= Count)
-            {
-                Array.Resize(ref _elements, (int)Count * 2);
-            }
-
-            _elements[Count++] = item;
+            EnsureCapacity(_count + 1);
+            _elements[_count++] = item;
         }
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public void Insert(int index, T item)
         {
-            Debug.Assert(index < Count);
-            if (Count == _elements.Length)
+            Debug.Assert(index < _count);
+            if (_count == _elements.Length)
             {
                 Array.Resize(ref _elements, _elements.Length * 2);
             }
 
-            if (index < Count)
+            if (index < _count)
             {
-                Array.Copy(_elements, index, _elements, index + 1, Count - index);
+                Array.Copy(_elements, index, _elements, index + 1, _count - index);
             }
 
             _elements[index] = item;
-            Count++;
+            _count++;
         }
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public void Remove(int index)
         {
-            Debug.Assert(index < Count);
-            Count--;
-            if (index < Count)
+            Debug.Assert(index < _count);
+            _count--;
+            if (index < _count)
             {
-                Array.Copy(_elements, index + 1, _elements, index, Count - index);
+                Array.Copy(_elements, index + 1, _elements, index, _count - index);
             }
         }
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Truncate(uint length)
+        {
+            static void outOfRange() => throw new ArgumentOutOfRangeException("length");
+
+            if (length > Count) outOfRange();
+            _count = length;
+        }
+
         public void Clear()
         {
-            Count = 0;
+            _count = 0;
         }
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public void AddRange(ReadOnlySpan<T> items)
         {
-            if (_elements == null)
-            {
-                _elements = new T[DefaultCapacity];
-            }
-
-            if (_elements.Length <= Count)
-            {
-                Array.Resize(ref _elements, (int)Count * 2);
-            }
-
+            EnsureCapacity((uint)(_count + items.Length));
             for (int i = 0; i < items.Length; i++)
             {
-                _elements[Count++] = items[i];
+                _elements[_count++] = items[i];
             }
+        }
+
+        public Span<T> Append(uint count)
+        {
+            EnsureCapacity(_count + count);
+            var span = new Span<T>(_elements, (int)_count, (int)count);
+            _count += count;
+            return span;
         }
 
         public void RemoveLast()
         {
-            if (Count > 0)
+            if (_count > 0)
             {
-                Count--;
+                _count--;
             }
         }
 
         public T[] ToArray()
         {
-            var copy = new T[Count];
-            Array.Copy(_elements, copy, Count);
+            var copy = new T[_count];
+            Array.Copy(_elements, copy, _count);
             return copy;
         }
 
-        public Span<T> AsSpan() => new Span<T>(_elements, 0, (int)Count);
+        public Span<T> AsSpan() => new Span<T>(_elements, 0, (int)_count);
         public Span<T> AsSpan(int start, int length) => new Span<T>(_elements, start, length);
-        public ReadOnlySpan<T> AsReadonlySpan() => new ReadOnlySpan<T>(_elements, 0, (int)Count);
+        public ReadOnlySpan<T> AsReadonlySpan() => new ReadOnlySpan<T>(_elements, 0, (int)_count);
         public ReadOnlySpan<T> AsReadonlySpan(int start, int length)
             => new ReadOnlySpan<T>(_elements, start, length);
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public void Reset()
         {
-            Count = 0;
+            _count = 0;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private void EnsureCapacity(uint requiredCapacity)
+        {
+            if (_elements.Length < requiredCapacity)
+            {
+                Grow(requiredCapacity);
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private void Grow(uint requiredCapacity)
+        {
+            uint newCapacity = Math.Max((uint)_elements.Length * 2, requiredCapacity);
+            Array.Resize(ref _elements, (int)newCapacity);
         }
     }
 }
