5d352e193db77ed34ed4f5d9d617b903445547bd
ym555 <mohamed.yoyo100@gmail.com>
2021-05-13 08:13:44 +0200

    OpenAL Backend

--- src/NitroSharp/Media/OpenAL/OpenALAudioSource.cs
+++ src/NitroSharp/Media/OpenAL/OpenALAudioSource.cs
@@ -1,4 +1,5 @@
 using System;
+using Microsoft.VisualStudio.Threading;
 using System.Runtime.InteropServices;
 using System.Threading;
 using System.IO.Pipelines;
@@ -12,45 +13,55 @@ namespace NitroSharp.Media.OpenAL
     {
         private readonly int _bufferSize;
         private readonly int _bufferPoolSize;
+
+        private readonly OpenALAudioDevice _audioDevice;
         private readonly uint _source;
         private readonly uint[] _buffers;
-        private int _nextBuffer;
-        private CancellationTokenSource _cts;
-        private OpenALAudioDevice _audioDevice;
-        private Task? _consumeTask;
-        private int _buffersLeft;
-        private IntPtr[] _bufferData;
+        private readonly int _format;
+
         private IntPtr _buffer;
-        private int[] _actualBufferSizes;
+        private IntPtr[] _bufferData;
+        private int _nextBuffer;
+        private int _buffersLeft;
+        private double _secondsElapsed;
+
+        private Task? _consumeTask;
+        private CancellationTokenSource _cts;
+        private readonly AsyncManualResetEvent _flushBuffers;
+        private readonly AsyncManualResetEvent _playSignal;
 
         public OpenALAudioSource(OpenALAudioDevice audioDevice, int bufferSize, int bufferCount)
         {
             _audioDevice = audioDevice;
             _bufferSize = bufferSize;
             _bufferPoolSize = bufferCount;
+            _buffersLeft = bufferCount;
+
             AL10.alGenSources(1, out _source);
             _buffers = new uint[bufferCount];
-            _bufferData = new IntPtr[bufferCount];
             AL10.alGenBuffers(bufferCount, _buffers);
-            _buffersLeft = bufferCount;
+            _format = ALFormat(AudioDevice.BitDepth, _audioDevice.AudioParameters.ChannelCount);
+
+            _bufferData = new IntPtr[bufferCount];
             _buffer = Marshal.AllocHGlobal(_bufferSize * _bufferPoolSize);
             for (int i = 0; i < _bufferPoolSize; i++)
             {
                 _bufferData[i] = IntPtr.Add(_buffer, i * _bufferSize);
             }
-            _actualBufferSizes = new int[bufferCount];
+
+            _flushBuffers = new();
+            _playSignal = new();
         }
 
         public override bool IsPlaying
         {
-            get
-            {
+            get {
                 AL10.alGetSourcei(_source, AL10.AL_SOURCE_STATE, out int state);
                 return state == AL10.AL_PLAYING;
             }
         }
 
-        public override double SecondsElapsed { get; }
+        public override double SecondsElapsed { get => _secondsElapsed; }
         public override float Volume
         {
             get
@@ -69,32 +80,69 @@ namespace NitroSharp.Media.OpenAL
         private async Task PlayAsync(PipeReader audioData)
         {
             await StopAsync();
-            AL10.alSourcePlay(_source);
+            _playSignal.Set();
             _cts = new CancellationTokenSource();
             _consumeTask = Task.Run(() => ConsumeLoop(audioData));
         }
 
         private async Task ConsumeLoop(PipeReader audioData)
         {
+            uint sampleRate = _audioDevice.AudioParameters.SampleRate;
+            Task flushEvent = _flushBuffers.WaitAsync();
             while (!_cts.IsCancellationRequested)
             {
                 ReadResult readResult = await audioData.ReadAsync();
-                BufferData(audioData, readResult);
-                // TODO
-                /* await Task.Delay(100); */
+                long bytesRead = BufferData(audioData, readResult);
+                double secondsRead = ((double) bytesRead / (AudioDevice.BitDepth / 8)) / sampleRate;
+                int milliseconds = (int) (secondsRead * 100 + 1);
+                _secondsElapsed += secondsRead;
+
+                try
+                {
+                    _playSignal.WaitAsync().Wait(_cts.Token);
+                } catch { }
+
+                if (flushEvent.Wait(milliseconds)) {
+                    AL10.alSourceStop(_source);
+                    UnqueueBuffers();
+
+                    _buffersLeft = _bufferPoolSize;
+                    _nextBuffer = 0;
+
+                    while (audioData.TryRead(out readResult))
+                    {
+                        audioData.AdvanceTo(readResult.Buffer.End);
+                        if (readResult.IsCompleted) { break; }
+                    }
+                    _flushBuffers.Reset();
+                    flushEvent = _flushBuffers.WaitAsync();
+                }
             }
         }
 
-        private unsafe void BufferData(PipeReader pipeReader, in ReadResult readResult)
+        private int ALFormat(int bitDepth, int channels) {
+            return bitDepth switch {
+                16 => channels > 1 ? AL10.AL_FORMAT_STEREO16 : AL10.AL_FORMAT_MONO16,
+                8 => channels > 1 ?  AL10.AL_FORMAT_STEREO8 : AL10.AL_FORMAT_MONO8,
+                _ => -1,
+            };
+        }
+
+        private int UnqueueBuffers() {
+            AL10.alGetSourcei(_source, AL10.AL_BUFFERS_PROCESSED, out int buffersProcessed);
+
+            uint[] processed = new uint[buffersProcessed];
+            AL10.alSourceUnqueueBuffers(_source, buffersProcessed, processed);
+
+            return buffersProcessed;
+        }
+
+        private unsafe long BufferData(PipeReader pipeReader, in ReadResult readResult)
         {
             ReadOnlySequence<byte> sequence = readResult.Buffer;
-            AL10.alGetSourcei(_source, AL10.AL_BUFFERS_PROCESSED, out int buffersProcessed);
-            uint[] processed = new uint[buffersProcessed];
+            _buffersLeft += UnqueueBuffers();
 
-            AL10.alSourceUnqueueBuffers(_source, buffersProcessed, processed);
-            _buffersLeft += buffersProcessed;
-
-            bool whatever = !sequence.IsEmpty &&  _buffersLeft > 0;
+            int bytesRead = 0;
             while (!sequence.IsEmpty &&  _buffersLeft > 0)
             {
                 int size = Math.Min((int)sequence.Length, _bufferSize);
@@ -103,38 +151,41 @@ namespace NitroSharp.Media.OpenAL
                 var dst = new Span<byte>(_bufferData[_nextBuffer].ToPointer(), size);
                 src.CopyTo(dst);
 
-                _actualBufferSizes[_nextBuffer] = size;
-                AudioParameters param = _audioDevice.AudioParameters;
-                AL10.alBufferData(_buffers[_nextBuffer], AL10.AL_FORMAT_STEREO16, _bufferData[_nextBuffer], size, (int) param.SampleRate);
-                int error = AL10.alGetError();
-                if (error != 0)
-                {
-                }
-
+                AL10.alBufferData(
+                        _buffers[_nextBuffer],
+                        _format,
+                        _bufferData[_nextBuffer],
+                        size,
+                        (int) _audioDevice.AudioParameters.SampleRate
+                );
                 AL10.alSourceQueueBuffers(_source, 1, ref _buffers[_nextBuffer]);
+
+                bytesRead += size;
                 _nextBuffer = (_nextBuffer + 1) % _buffers.Length;
                 _buffersLeft--;
             }
-            // TODO
-            if (whatever)
-            {
-                if (this.IsPlaying == false)
-                {
-                    AL10.alSourcePlay(_source);
-                }
+
+            if (!this.IsPlaying) {
+                AL10.alSourcePlay(_source);
             }
 
             pipeReader.AdvanceTo(sequence.Start);
+            return bytesRead;
         }
 
         public override void Pause()
         {
-            AL10.alSourcePause(_source);
+            if (_consumeTask != null) {
+                _playSignal.Reset();
+                AL10.alSourcePause(_source);
+            }
         }
 
         public override void Resume()
         {
-            AL10.alSourcePlay(_source);
+            if (_consumeTask != null) {
+                _playSignal.Set();
+            }
         }
 
         public override void Stop()
@@ -144,39 +195,48 @@ namespace NitroSharp.Media.OpenAL
 
         private async Task StopAsync()
         {
-            if (_cts  != null)
-            {
-                _cts.Cancel();
-            }
             if (_consumeTask != null)
             {
+                _cts.Cancel();
+                FlushBuffers();
                 await _consumeTask;
+                _consumeTask = null;
+                _secondsElapsed = 0;
+                AL10.alSourceStop(_source);
             }
-            AL10.alSourcePause(_source);
-            AL10.alSourceStop(_source);
         }
 
-        // TODO
         public override void FlushBuffers()
         {
+            if (_consumeTask != null) {
+                _flushBuffers.Set();
+            }
         }
 
         public override unsafe ReadOnlySpan<short> GetCurrentBuffer()
         {
-            int currentBuffer = (_nextBuffer - 1 + _buffersLeft) % _bufferPoolSize;
-            if (currentBuffer < 0) return default;
-            int size = _actualBufferSizes[currentBuffer];
+            int currentBuffer = (_nextBuffer + _buffersLeft) % _bufferPoolSize; // Is a lock needed here?
+            if (!this.IsPlaying) return default;
+            AL10.alGetBufferi(_buffers[currentBuffer], AL10.AL_SIZE, out int size);
             return new ReadOnlySpan<short>(_bufferData[currentBuffer].ToPointer(), size / 2);
         }
 
-        public override ValueTask DisposeAsync()
+        public override async ValueTask DisposeAsync()
         {
+            if (_consumeTask is not null)
+            {
+                await StopAsync();
+            }
+
             if (_buffer != IntPtr.Zero)
             {
                 Marshal.FreeHGlobal(_buffer);
                 _buffer = IntPtr.Zero;
+
+                AL10.alDeleteBuffers(_bufferPoolSize, _buffers);
+                uint source = _source;
+                AL10.alDeleteSources(1, ref source);
             }
-            return new ValueTask(null);
         }
     }
 }
