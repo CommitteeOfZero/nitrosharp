2bef2765bafe2974f8bae819f483f42346e36539
Eugene Ivchenko <goldenjoe@outlook.com>
2021-08-27 03:10:03 +0300

    Add missing file

---
+++ src/NitroSharp/Content/ArchiveFile.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using NitroSharp.Utilities;
+
+namespace NitroSharp.Content
+{
+    internal abstract class ArchiveFile : IDisposable
+    {
+        public abstract Stream OpenStream(string path);
+        public abstract bool Contains(string path);
+        public abstract void Dispose();
+    }
+
+    internal sealed class VfsNode : IDisposable
+    {
+        public SmallList<ArchiveFile> MountedArchives;
+        public Dictionary<string, VfsNode>? Children;
+
+        public void Dispose()
+        {
+            if (Children is not null)
+            {
+                foreach (VfsNode node in Children.Values)
+                {
+                    node.Dispose();
+                }
+            }
+            foreach (ArchiveFile mountedArchive in MountedArchives.AsSpan())
+            {
+                mountedArchive.Dispose();
+            }
+        }
+    }
+
+    internal sealed class ArchiveException : Exception
+    {
+        public ArchiveException()
+            : base("Unable to open the archive")
+        {
+        }
+
+        public ArchiveException(string format, string message)
+            : base($"{format} : {message}")
+        {
+        }
+    }
+}
